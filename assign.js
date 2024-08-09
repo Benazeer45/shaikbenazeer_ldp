281494b355b34c211cde78fc995e58806e4ea489
Shaik Benazeer <shaik.benazeer@zemosolabs.com>
2024-08-09 15:57:48 +0530

    updated the assignment by using async and await

--- assign.js
+++ assign.js
@@ -1,14 +1,32 @@
-function getData(uId) {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        console.log("Fetched the data!");
-        resolve("skc@gmail.com");
-      }, 4000);
-    });
-  }
+// function getData(uId) {
+//     return new Promise((resolve) => {
+//       setTimeout(() => {
+//         console.log("Fetched the data!");
+//         resolve("skc@gmail.com");
+//       }, 4000);
+//     });
+//   }
   
-  console.log("start");
-  getData("skc").then(email => {
-    console.log("Email id of the user id is: " + email);
+//   console.log("start");
+//   getData("skc").then(email => {
+//     console.log("Email id of the user id is: " + email);
+//   });
+//   console.log("end");
+
+function getData(uId) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Fetched the data!");
+      resolve("skc@gmail.com");
+    }, 4000);
   });
+}
+
+async function displayEmail() {
+  console.log("start");
+  let email = await getData("skc");
+  console.log("Email id of the user id is: " + email);
   console.log("end");
+}
+
+displayEmail();
