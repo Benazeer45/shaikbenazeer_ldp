ffbb88610cf05751ac10ee116db698996d2df27b
Shaik Benazeer <shaik.benazeer@zemosolabs.com>
2024-10-07 15:24:08 +0530

    Remove unnecessary files from SOLID_Demo

--- Java_Learnings/Java_Pocs/SOLID_Demo/Light.java
+++
@@ -1,8 +0,0 @@
-package SOLID_Principles.SOLID_Demo;
-
-// SRP: Light interface has only one responsibility - defining light behavior
-// DIP: Light interface is an abstraction that can be implemented by various types of lights
-public interface Light {
-    void turnOn();
-    void turnOff();
-}
