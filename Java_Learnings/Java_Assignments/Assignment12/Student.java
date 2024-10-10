afb477b2dd7194a064bbc93f478a906c1177d1d8
Shaik Benazeer <shaik.benazeer@zemosolabs.com>
2024-10-10 14:24:27 +0530

    Java assignment 12 on Stream API

---
+++ Java_Learnings/Java_Assignments/Assignment12/Student.java
@@ -0,0 +1,61 @@
+package Java_Assignments.Assignment12;
+
+import java.util.Objects;
+
+public class Student {
+//    private final int id;
+//    private final String name;
+//    private final int age;
+//    private final String gender;
+//    private final String engDepartment;
+//    private final int yearOfEnrollment;
+//    private final double perTillDate;
+
+    public int id;
+    public String name;
+    public int age;
+    public String gender;
+    public String engDepartment;
+    public int yearOfEnrollment;
+    public double perTillDate;
+
+    public Student(int id, String name, int age, String gender, String engDepartment, int yearOfEnrollment, double perTillDate) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.engDepartment = engDepartment;
+        this.yearOfEnrollment = yearOfEnrollment;
+        this.perTillDate = perTillDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getEngDepartment() {
+        return engDepartment;
+    }
+
+    public int getYearOfEnrollment() {
+        return yearOfEnrollment;
+    }
+
+    public double getPerTillDate() {
+        return perTillDate;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getDetails() {
+        return String.format("ID: %d, Name: %s, Age: %d, Gender: %s, Department: %s, Year: %d, Percentage: %.2f",
+                id, name, age, gender, engDepartment, yearOfEnrollment, perTillDate);
+    }
+}
+
