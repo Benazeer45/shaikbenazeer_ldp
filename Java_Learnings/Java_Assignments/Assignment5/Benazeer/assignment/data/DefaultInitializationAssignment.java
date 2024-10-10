5e80a6e7f154fdd00f554bca7a8a27cf17687240
Shaik Benazeer <shaik.benazeer@zemosolabs.com>
2024-10-10 09:49:28 +0530

    Updated singleton for other types and removed object

--- Java_Learnings/Java_Assignments/Assignment5/Benazeer/assignment/data/DefaultInitializationAssignment.java
+++ Java_Learnings/Java_Assignments/Assignment5/Benazeer/assignment/data/DefaultInitializationAssignment.java
@@ -3,20 +3,34 @@ package Java_Assignments.Assignment5.Benazeer.assignment.data;
 public class DefaultInitializationAssignment {
     int num;
     char letter;
+    String text;
+    double decimal;
+    boolean flag;
 
+    // Method to print instance variables with default values
     public void printVariables() {
-        System.out.println("Integer: " + num); // prints default value (0)
-        System.out.println("Character: " + letter); // prints default value (empty char)
+        System.out.println("Integer: " + num);        // Default: 0
+        System.out.println("Character: " + letter);   // Default: empty char (Unicode '\u0000')
+        System.out.println("String: " + text);        // Default: null
+        System.out.println("Double: " + decimal);     // Default: 0.0
+        System.out.println("Boolean: " + flag);       // Default: false
     }
 
     public void printLocalVariables() {
         // Local variables must be initialized before use, hence the following lines would cause a compile error.
         // int localInt; // uninitialized local variable
         // char localChar; // uninitialized local variable
-        // System.out.println("Local Integer: " + localInt); // This will cause an error, Error: localInt might not have been initialized
-        // System.out.println("Local Character: " + localChar); // This will cause an error, Error: localChar might not have been initialized
+        // String localText; // uninitialized local variable
+        // double localDecimal; // uninitialized local variable
+        // boolean localFlag; // uninitialized local variable
+        // System.out.println("Local Integer: " + localInt); // This will cause an error
+        // System.out.println("Local Character: " + localChar); // This will cause an error
+        // System.out.println("Local String: " + localText); // This will cause an error
+        // System.out.println("Local Double: " + localDecimal); // This will cause an error
+        // System.out.println("Local Boolean: " + localFlag); // This will cause an error
+
         /*
-         * Local variables must be initialized before they are used. otherwise, a compile-time error will occur.
+         * Local variables must be initialized before they are used. Otherwise, a compile-time error will occur.
          * In Java, local variables do not have default values and must be assigned a value before being accessed.
          */
     }
--- Java_Learnings/Java_Assignments/Assignment5/Benazeer/assignment/singleton/SingletonExample.java
+++ Java_Learnings/Java_Assignments/Assignment5/Benazeer/assignment/singleton/SingletonExample.java
@@ -1,17 +1,20 @@
 package Java_Assignments.Assignment5.Benazeer.assignment.singleton;
 
 public class SingletonExample {
+    private static SingletonExample instance = null;  // Static instance variable (lazy initialization)
     private String message;  // Non-static String member variable
 
-    // Static method to create and initialize the SingletonExample instance
-    // Static method to initialize the member variable and return an instance of the class
+    // Private constructor to prevent instantiation from outside
+    private SingletonExample() {}
+
     public static SingletonExample createInstance(String msg) {
-        SingletonExample instance = new SingletonExample();
-        instance.message = msg;
-        return instance;
+        if (instance == null) { 
+            instance = new SingletonExample();  
+            instance.message = msg;  
+        }
+        return instance;  
     }
 
-    // Non-static method to print the message
     public void printMessage() {
         System.out.println("Message: " + message);
     }
