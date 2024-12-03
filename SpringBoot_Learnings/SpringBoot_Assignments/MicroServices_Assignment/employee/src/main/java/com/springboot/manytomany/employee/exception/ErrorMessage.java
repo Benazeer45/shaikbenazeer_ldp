b30932c1556d91ad9f0eb078b3f661271737892d
Benazeer45 <shaik.benazeer@zemosolabs.com>
2024-12-03 14:59:45 +0530

    Used getters and setters

--- SpringBoot_Learnings/SpringBoot_Assignments/MicroServices_Assignment/employee/src/main/java/com/springboot/manytomany/employee/exception/ErrorMessage.java
+++ SpringBoot_Learnings/SpringBoot_Assignments/MicroServices_Assignment/employee/src/main/java/com/springboot/manytomany/employee/exception/ErrorMessage.java
@@ -1,33 +1,19 @@
 package com.spring.jpa.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorMessage {
-  private int statusCode;
-  private Date timestamp;
-  private String message;
-  private String description;
-
-  public ErrorMessage(int statusCode, Date timestamp, String message, String description) {
-    this.statusCode = statusCode;
-    this.timestamp = timestamp;
-    this.message = message;
-    this.description = description;
-  }
-
-  public int getStatusCode() {
-    return statusCode;
-  }
-
-  public Date getTimestamp() {
-    return timestamp;
-  }
-
-  public String getMessage() {
-    return message;
-  }
-
-  public String getDescription() {
-    return description;
-  }
+    private int statusCode;
+    private Date timestamp;
+    private String message;
+    private String description;
 }
